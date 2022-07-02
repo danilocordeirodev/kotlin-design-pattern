79f0b764619b2f56553c51cb315ee62a70a90d93
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2022-07-02 08:16:38 -0300

    Add test dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.assertj:assertj-core:3.23.1")
 }
 
 tasks.test {
